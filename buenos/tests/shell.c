d48a9f23fadb020f4a576f81149387a7e47ade98
Antti Risteli <antti.risteli@aalto.fi>
2014-03-06 17:06:40 +0200

    Add backspace support to shell

--- buenos/tests/shell.c
+++ buenos/tests/shell.c
@@ -2,21 +2,51 @@
 
 char buffer[512];
 int main(void) {
-    int read, i, oldread;
+    int read, i, deletedchars;
     char retval_buf[64];
+    int startbg;
     read = 0;
     while(1) {
+        startbg = 0;
         if (read == 512) {
             read--;
         }
-        oldread = read;
         read += syscall_read(stdin, buffer + read, 512 - read);
-        syscall_write(stdout, buffer + oldread, read - oldread);
+         
+        deletedchars = 0;
         for (i = 0; i < read; i++) {
-            if (buffer[i] == 13) {  
+            if (buffer[i] == 127) {
+                int j;
+                for (j = i + 1; j < read; j++) {
+                    buffer[j - 2] = buffer[j];
+                }
+                read -= 2;
+                deletedchars++;
+            }
+        }
+        read = (read < 0) ? 0 : read;
+        
+        prints("\r");
+        syscall_write(stdout, buffer, read);
+        for (i = 0; i < deletedchars; i++) {
+            prints(" ");
+        }
+        prints("\r");
+        syscall_write(stdout, buffer, read);
+        for (i = 0; i < read; i++) {
+            if (buffer[i] == '&') {
+                startbg = 1;
+            } else if (buffer[i] == 13) {  
+                int j;
                 buffer[i] = '\0';
                 read = 0;
                 prints("\n");
+                // TODO: parse parameters properly
+                for (j = 0; j < i; j++) {
+                    if (buffer[j] == ' ') {
+                        buffer[j] = 0;
+                    }
+                }
                 int pid = syscall_exec(buffer);
                 if (pid < 0) {
                     prints("failed to start process: "); 
@@ -24,13 +54,15 @@ int main(void) {
                     prints(retval_buf);
                     prints("\n");
                 } else {
-                    // TODO: parse & if we want to run in the background
-                    int retval = syscall_join(pid);
-                    prints("return value: "); 
-                    itoa(retval, retval_buf);
-                    prints(retval_buf);
-                    prints("\n");
+                    if (!startbg) {
+                        int retval = syscall_join(pid);
+                        prints("return value: "); 
+                        itoa(retval, retval_buf);
+                        prints(retval_buf);
+                        prints("\n");
+                    }
                 }
+                break;
             }
         }
     }
