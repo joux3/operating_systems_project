5a159bf401f99660aba180019c03bb5da250e76b
Antti Risteli <antti.risteli@aalto.fi>
2014-03-08 14:20:45 +0200

    Refactor shell

--- buenos/tests/shell.c
+++ buenos/tests/shell.c
@@ -1,70 +1,89 @@
 #include "tests/lib.h"
 
-char buffer[512];
+char linebuffer[512];
+char readbuffer[5];
+int linelength; 
+char retval_buf[64];
+
+void run_linebuffer() {
+    int i;
+    int startbg = 0;
+
+    for (i = 0; i < linelength; i++) {
+        if (linebuffer[i] == '&') {
+            startbg = 1;
+        } 
+    }
+
+    int j;
+    linebuffer[linelength++] = '\0';
+    prints("\n");
+    // TODO: parse parameters properly
+    for (j = 0; j < i; j++) {
+        if (linebuffer[j] == ' ') {
+            linebuffer[j] = 0;
+        }
+    }
+
+    linelength = 0;
+
+    int pid = syscall_exec(linebuffer);
+    if (pid < 0) {
+        prints("failed to start process: "); 
+        itoa(pid, retval_buf);
+        prints(retval_buf);
+    } else {
+        if (!startbg) {
+            int retval = syscall_join(pid);
+            prints("return value: "); 
+            itoa(retval, retval_buf);
+            prints(retval_buf);
+        } else {
+            prints("started process "); 
+            itoa(pid, retval_buf);
+            prints(retval_buf);
+            prints(" in background"); 
+        }
+    }
+}
+
 int main(void) {
-    int read, i, deletedchars;
-    char retval_buf[64];
-    int startbg;
-    read = 0;
+    int i;
+    linelength = 0;
     prints("> ");
     while(1) {
-        startbg = 0;
-        if (read == 512) {
-            read--;
-        }
-        read += syscall_read(stdin, buffer + read, 512 - read);
+        int runcommand = 0;
+        int deletedchars = 0;
+        int read = syscall_read(stdin, readbuffer, 5);
          
         deletedchars = 0;
         for (i = 0; i < read; i++) {
-            if (buffer[i] == 127) {
-                int j;
-                for (j = i + 1; j < read; j++) {
-                    buffer[j - 2] = buffer[j];
+            if (readbuffer[i] == 127) {
+                if (linelength > 0) {
+                    linelength--;
+                    deletedchars++;
                 }
-                read -= 2;
-                deletedchars++;
-            }
-        }
-        read = (read < 0) ? 0 : read;
-        
-        prints("\r> ");
-        syscall_write(stdout, buffer, read);
-        for (i = 0; i < deletedchars; i++) {
-            prints(" ");
-        }
-        prints("\r> ");
-        syscall_write(stdout, buffer, read);
-        for (i = 0; i < read; i++) {
-            if (buffer[i] == '&') {
-                startbg = 1;
-            } else if (buffer[i] == 13) {  
-                int j;
-                buffer[i] = '\0';
-                read = 0;
-                prints("\n");
-                // TODO: parse parameters properly
-                for (j = 0; j < i; j++) {
-                    if (buffer[j] == ' ') {
-                        buffer[j] = 0;
-                    }
-                }
-                int pid = syscall_exec(buffer);
-                if (pid < 0) {
-                    prints("failed to start process: "); 
-                    itoa(pid, retval_buf);
-                    prints(retval_buf);
-                    prints("\n> ");
-                } else {
-                    if (!startbg) {
-                        int retval = syscall_join(pid);
-                        prints("return value: "); 
-                        itoa(retval, retval_buf);
-                        prints(retval_buf);
-                        prints("\n> ");
-                    }
-                }
+            } else if (readbuffer[i] == 13) {
+                runcommand = 1;
                 break;
+            } else {
+                if (linelength < 511) {
+                    linebuffer[linelength++] = readbuffer[i];
+                }
             }
+        }
+        
+        if (runcommand) {
+            run_linebuffer();
+            prints("\n> ");
+        } else {
+            prints("\r> ");
+            syscall_write(stdout, linebuffer, linelength);
+            for (i = 0; i < deletedchars; i++) {
+                prints(" ");
+            }
+            prints("\r> ");
+            syscall_write(stdout, linebuffer, linelength);
         }
     }
     return 0;
