1c8f5043bc6b891d5dbca77531ba61a272467609
Antti Risteli <antti.risteli@aalto.fi>
2014-03-10 11:23:22 +0200

    Add stress exec test

---
+++ buenos/tests/stressexec.c
@@ -0,0 +1,27 @@
+#include "tests/lib.h"
+
+int main(void) {
+    char* str = "balalaikka";
+    char* str2 = "ripkukkoparssinen";
+    char* ptrs[2];
+    ptrs[0] = str;
+    ptrs[1] = str2;
+    char argc = 2;
+    char retvalbuf[52];
+    int i = 0;
+
+    while(1) {
+        int pid = syscall_execp("[testi]argcat", argc, (const char**)ptrs);
+        int retval = syscall_join(pid);
+        prints("return value ");
+        itoa(retval, retvalbuf); 
+        prints(retvalbuf);
+        prints("\n");
+        itoa(i, retvalbuf); 
+        prints(retvalbuf);
+        prints("\n");
+        i++;
+    }
+    prints("exited ?!?!?!\n");
+    return 5;
+}
