1d5ef2a4da9d5e22a67322066dd19279e8e77d08
Miro Nurmela <nurmelm1@remorse.niksula.hut.fi>
2014-03-30 12:56:33 +0300

    Added sockets to the test

--- buenos/kernel_tests/nic_test.c
+++ buenos/kernel_tests/nic_test.c
@@ -5,15 +5,25 @@
 #include "drivers/device.h"
 #include "drivers/gnd.h"
 #include "net/network.h"
+#include "net/pop.h"
+#include "kernel/assert.h"
 
 void nic_test_main(void) {
-    void *buffer = 0x0;
-    network_send(network_get_broadcast_address(),
-                 1,
-                 1,
-                 1,
-                 buffer);
+    int data = 1;
+    void *buffer = &data;
+    network_address_t addr = 0x0f01beef;
+    network_address_t broadcast = network_get_broadcast_address();
+    network_address_t loopback = network_get_loopback_address();
+    broadcast = loopback;
+    loopback = broadcast;
+    uint16_t sport = 1;
+    uint16_t rport = 2;
+    sock_t send_socket = socket_open(0x01, sport);
+    sock_t recv_socket = socket_open(0x01, rport);
+    KERNEL_ASSERT(send_socket >= 0 && recv_socket >= 0);
     kprintf("Starting NIC test\n");
+    socket_sendto(send_socket, addr, rport, buffer, sizeof(int));
+    socket_recvfrom(recv_socket, &addr, &sport, buffer, sizeof(int), &data);
     while(1) {};
 }
 
